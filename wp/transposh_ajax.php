25c2f22df3b1b483bfaab6e0107c2db25726fecc
sirikorng <pare.certificate@gmail.com>
2018-07-08 04:08:21 +0700

    add new files

---
+++ wp/transposh_ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Transposh v1.0.1.1
+ * http://transposh.org/
+ *
+ * Copyright 2018, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date: Wed, 27 Jun 2018 14:41:18 +0300
+ */
+
+/*
+ * This file handles various AJAX needs of our plugin
+ */
+
+echo "This file was removed in 0.8.0 and is only kept for debugging";
+
+?>
